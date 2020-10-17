58b08b021c03d28d26f38f909500ecf288f355d7
Bertha Teresa Peña Rivera <bertha.pena@fisglobal.com>
2020-10-16 20:07:52 -0500

    first commit

---
+++ src/test/java/TC01.java
@@ -0,0 +1,15 @@
+import io.appium.java_client.ios.IOSDriver;
+
+import java.net.MalformedURLException;
+
+public class TC01 extends Base{
+    public static void main(String[] args) throws MalformedURLException {
+        IOSDriver driver = caps();
+
+        driver.findElementByAccessibilityId("Alert Views").click();
+        driver.findElementByXPath("//XCUIElementTypeStaticText[@name='Text Entry']").click();
+        driver.findElementByClassName("XCUIElementTypeTextField").sendKeys("hello");
+        driver.findElementByName("OK").click();
+
+    }
+}
