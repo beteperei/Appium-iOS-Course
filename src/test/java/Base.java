7b53a5940aeb077c0e15bfa0b72946e1119eb73f
Bertha Pena <bertha@Berthas-MacBook-Pro.local>
2021-12-07 12:47:54 -0500

    base refactor

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,30 +1,30 @@
 import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
+
 
 public class Base {
-    static IOSDriver<IOSElement> driver;
 
-    public static IOSDriver<IOSElement> caps() throws MalformedURLException {
-        DesiredCapabilities d = new DesiredCapabilities();
-        d.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11 Pro");
-        d.setCapability(MobileCapabilityType.PLATFORM_NAME,"iOS");
-        d.setCapability(MobileCapabilityType.PLATFORM_VERSION,"13.3");
-        d.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
-        d.setCapability(MobileCapabilityType.APP,"//Users//bpena//Documents//Automation//UIKitCatalog.app");
+    public static void main(String[] args) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 13");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"15.0");
 
-        String url = "http://127.0.0.1:4723/wd/hub";
-        driver = new IOSDriver(new URL(url),d);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        return driver;
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+        capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT,500000);
+        capabilities.setCapability("commandTimeouts","12000");
+
+        capabilities.setCapability(MobileCapabilityType.APP,"Users/bertha/Documents/Proyectos/Automation/Java/UIKitCatalog.app");
+
+        String url = "http://localhost:4723/wd/hub";
+
+        IOSDriver driver = new IOSDriver(new URL(url),capabilities);
+
     }
-
-
 
 }
