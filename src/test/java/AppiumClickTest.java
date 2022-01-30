0b3d27de59c0255592ca8a5d86a1c72f2f21d2b8
Bertha Pena <bertha@Berthas-MacBook-Pro.local>
2022-01-30 18:55:15 -0500

    adding stuff

--- src/test/java/AppiumClickTest.java
+++ src/test/java/AppiumClickTest.java
@@ -2,14 +2,20 @@ import io.appium.java_client.ios.IOSDriver;
 
 import java.net.MalformedURLException;
 
-public class TC01_SendKeys extends Base{
+public class AppiumClickTest extends Base{
     public static void main(String[] args) throws MalformedURLException {
+
         IOSDriver driver = desiredCapabilities();
 
         driver.findElementByAccessibilityId("Alert Views").click();
-        driver.findElementByXPath("//XCUIElementTypeStaticText[@name='Text Entry']").click();
+
+        /*driver.findElementByXPath("//XCUIElementTypeStaticText[@name='Text Entry']").click();
         driver.findElementByClassName("XCUIElementTypeTextField").sendKeys("hello");
-        driver.findElementByAccessibilityId("OK").click();
+        driver.findElementByXPath("//XCUIElementTypeButton[@name='Cancel']").click();*/
+
+        driver.findElementByAccessibilityId("Confirm / Cancel").click();
+        System.out.println(driver.findElementByXPath("//*[contains(@name,'message')]").getText());
+        driver.findElementByXPath("//XCUIElementTypeButton[@name='Confirm']").click();
 
     }
 }
--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -8,22 +8,23 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 
-public class Base {
+public class Base implements appLocalVariables {
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static IOSDriver desiredCapabilities() throws MalformedURLException {
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 13");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"15.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 12-2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"14.5");
 
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
         capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT,500000);
         capabilities.setCapability("commandTimeouts","12000");
 
-        capabilities.setCapability(MobileCapabilityType.APP,"UIKitCatalog.app");
-
+        capabilities.setCapability(MobileCapabilityType.APP,app_local_url);
         String url = "http://localhost:4723/wd/hub";
 
         IOSDriver driver = new IOSDriver(new URL(url),capabilities);
+        return driver;
 
     }
 
