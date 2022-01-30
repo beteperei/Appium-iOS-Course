73054a316abe1cb920852c2581a3c8a840db9b8f
Bertha Pena <bertha@Berthas-MacBook-Pro.local>
2022-01-30 18:51:20 -0500

    Refactoring classes

--- src/test/java/AppiumClickTest.java
+++ src/test/java/AppiumClickTest.java
@@ -4,12 +4,12 @@ import java.net.MalformedURLException;
 
 public class TC01_SendKeys extends Base{
     public static void main(String[] args) throws MalformedURLException {
-        IOSDriver driver = caps();
+        IOSDriver driver = desiredCapabilities();
 
         driver.findElementByAccessibilityId("Alert Views").click();
         driver.findElementByXPath("//XCUIElementTypeStaticText[@name='Text Entry']").click();
         driver.findElementByClassName("XCUIElementTypeTextField").sendKeys("hello");
-        driver.findElementByName("OK").click();
+        driver.findElementByAccessibilityId("OK").click();
 
     }
 }
